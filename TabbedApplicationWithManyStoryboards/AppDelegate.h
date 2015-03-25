50a3a8f22cffa9d3686ed1c0524f743982de810c
RodrigoSoldi <rodrigosoldi@icloud.com>
2015-03-25 14:09:42 -0300

    first commit

---
+++ TabbedApplicationWithManyStoryboards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabbedApplicationWithManyStoryboards
+//
+//  Created by Rodrigo Soldi Lopes on 25/03/15.
+//  Copyright (c) 2015 Rodrigo Soldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
